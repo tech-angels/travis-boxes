1358c955cce4c948febbad98be0e4667188f2e02
Gilbert Roulot <gilbert.roulot@tech-angels.com>
2013-05-15 17:08:23 +0200

    Build base openvz template with veewee

--- lib/travis/boxes/cli/veewee.rb
+++ lib/travis/boxes/cli/veewee.rb
@@ -23,6 +23,20 @@ module Travis
           sh
         end
 
+        desc 'build_openvz', 'Build a base openvz template from a veewee definition(eg. oneiric32.tar)'
+        method_option :definition, :aliases => '-d', :default => 'oneiric32', :desc => 'Definition to build the base box from (e.g. oneiric32)'
+        method_option :upload,     :aliases => '-u', :desc => 'Upload the box'
+
+        def build_openvz
+          run <<-sh
+            veewee openvz build '#{definition}' --force
+            veewee openvz export '#{definition}'
+            mkdir -p boxes
+            mv '#{definition}.tar' 'boxes/#{definition}.tar'
+          sh
+        end
+
+
         desc 'upload', 'Upload a base box'
         method_option :definition, :aliases => '-d', :default => 'oneiric32', :desc => 'Definition of the box to upload (e.g. oneiric32)'
 
@@ -30,6 +44,15 @@ module Travis
           remote = ::Travis::Boxes::Remote.new
           remote.upload("boxes/#{definition}.box", "bases/#{definition}.box")
         end
+
+        desc 'upload_openvz', 'Upload a base openvz box'
+        method_option :definition, :aliases => '-d', :default => 'oneiric32', :desc => 'Definition of the box to upload (e.g. oneiric32)'
+
+        def upload_openvz
+          remote = ::Travis::Boxes::Remote.new
+          remote.upload("boxes/#{definition}.tar", "bases/#{definition}.tar")
+        end
+
 
         protected
 
